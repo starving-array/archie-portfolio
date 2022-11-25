f8db912a81f2f3bc14f84f7fbd819f01ca00e389
starving-array <gowork.archis@gmail.com>
2022-11-25 22:16:35 +0530

    project added

--- js/projects.js
+++ js/projects.js
@@ -2,6 +2,7 @@ let projectDiv = document.getElementById("projectContainer");
 
 //
 let projectBanner = [
+  "./img/E-Commerce.png",
   "./img/bus_reservation_system.png",
   "./img/geekbuy.png",
   "./img/mcdonals.png",
@@ -9,6 +10,7 @@ let projectBanner = [
   "./img/weather.png",
 ];
 let webLink = [
+  "https://github.com/starving-array/E-Commerce-Rest-API",
   "https://drive.google.com/file/d/1l0FwiGVOrkwyMEDJ-uoaQBEkUGe3x2Bs/view?usp=sharing",
   "https://geek-buying-cloned.netlify.app/",
   "https://virtual-mc-donald.netlify.app/",
@@ -16,6 +18,7 @@ let webLink = [
   "https://weather-archie.netlify.app/",
 ];
 let gitLink = [
+  "https://github.com/starving-array/E-Commerce-Rest-API",
   "https://github.com/starving-array/Bus-Reservation-System",
   "https://github.com/starving-array/geekbuying.com",
   "https://github.com/starving-array/virtual_Mcdonald",
@@ -23,6 +26,7 @@ let gitLink = [
   "https://github.com/starving-array/weatherApp",
 ];
 let title = [
+  " E-Commerce-Rest-API",
   "Bus Reservation System",
   "GeekBuy Clone",
   "Virtual McDonald's",
@@ -31,6 +35,7 @@ let title = [
 ];
 
 let textDescription = [
+  "This is a E-Commerce App which provide web service. This service can be used to generate REST API. User can sign in, sign up, search a product and buy it. Also with admin access, new product can be added and report can be generated to look upon sales.",
   "This Web-application will let A User Sign-In into the portal and to search available buses for particular destination. Its a rest API project. I have worked <span>Mapping, DL Layer </span>and <span>ER Diagram.",
   "This is clone of Geekbuying.com- e commerce website. I built the <span>header</span> section, <span>search</span>, and <span>all product</span> page. P.s - the currency option converts all the product currency according to selection.",
   "This is a conceptual design of McDonald's counter. I Have created this with the help of <span>promise</span> concept. The order will be processing based on <span>multithreading</span> idea.",
@@ -46,7 +51,15 @@ let techStackImage = [
     "./img/github.png",
     "./img/api.png",
     "./img/mysql.png",
+    "./img/maven.png",
+  ],
+  [
+    "./img/java.png",
+    "./img/sts.jpg",
     "./img/hibernate.png",
+    "./img/github.png",
+    "./img/api.png",
+    "./img/mysql.png",
     "./img/maven.png",
     "./img/bootstrap.png",
     "./img/html-5.png",
